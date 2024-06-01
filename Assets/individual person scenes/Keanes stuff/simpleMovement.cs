4ef42dcbc0d83db8981cfe8258228b5eb939eeba
bushmonkey140 <Bushmonkey140@gmail.com>
2024-06-01 15:00:21 -0400

    simplemovement

--- Assets/individual person scenes/Keanes stuff/simpleMovement.cs
+++ Assets/individual person scenes/Keanes stuff/simpleMovement.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Simplemovement : MonoBehaviour
+public class simpleMovement : MonoBehaviour
 {
     public float speed;
     private float Move;
     public float jumpHeight;
 
     private Rigidbody2D rb;
+    public bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +22,19 @@ public class Simplemovement : MonoBehaviour
     {
         Move = Input.GetAxis("Horizontal");
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space)&&isGrounded) {
             rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+            isGrounded = false;
         };
 
         rb.velocity = new Vector2(Move * speed, rb.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Ground")
+        {
+            isGrounded = true;
+        }
     }
 }
